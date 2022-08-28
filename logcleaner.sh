c3e10dfeed1a4a31e2755f0900b772f05ffe6849
Arthur <arthur.henrique@mblabs.com.br>
2022-08-28 16:45:30 -0300

    feat: log_folder variable

--- logcleaner.sh
+++ logcleaner.sh
@@ -16,9 +16,11 @@ LOG_FOLDER="/var/log"
 SIZE=$(sudo du -h -d 0 $LOG_FOLDER | cut -f1)
 zenity --width=400 --timeout=1800 --question --text="Estamos usando $SIZE de espaço para logs na partição /var.\nDeseja liberar esse espaço, apagando-os permanentemente?"
 if [[ $? = 0 ]]; then
-    sudo rm ./var/log/*
+    sudo rm $LOG_FOLDER/*
+    zenity --info --width=280 --timeout=4 --text="Seus logs foram apagados com sucesso!"
 elif [[ $? = 1 ]]; then
-    zenity --info --width=280 --timeout=4 --text="Seus logs não foram apagados!"
+    zenity --info --width=280 --timeout=4 --text="Seus logs não foram apagados..."
 elif [[ $? = 5 ]]; then
-    rm ./test-logs/*
+    rm $LOG_FOLDER/*
+    zenity --info --width=280 --timeout=4 --text="Seus logs foram apagados com sucesso!"
 fi
